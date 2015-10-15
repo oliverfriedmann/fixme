f5effd224362067185da7227f307ddc842d92ba1
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-10-15 07:50:29 -0400

    Fix: Grunt

---
+++ db/grunt.js
@@ -0,0 +1,12 @@
+module.exports = [{
+	
+	"title": "Grunt without local NPM",
+	"description": "You forgot to npm install it.",
+	"commands": function (descriptor) {
+		return ["npm install", descriptor.command];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("grunt") >= 0 && descriptor.stdout.indexOf("Fatal error: Unable to find local grunt.") >= 0; 
+	}
+	
+}];
