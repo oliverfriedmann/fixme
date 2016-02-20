ad85d93c1aadbd8f31f145350c13a3c2090842a9
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-02-20 12:28:24 -0500

    rm directory

--- db/rm.js
+++ db/rm.js
@@ -9,4 +9,15 @@ module.exports = [{
 		return descriptor.command.indexOf("rm") >= 0 && descriptor.stderr.indexOf("Permission denied") >= 0; 
 	}
 	
+}, {
+	
+	"title": "rm directory without -rf",
+	"description": "You might want to -rf it.",
+	"commands": function (descriptor) {
+		return ["rm -r -f " + descriptor.command.substring(2)];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("rm") >= 0 && descriptor.stderr.indexOf("is a directory") >= 0; 
+	}
+	
 }];
