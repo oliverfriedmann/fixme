12e1623184d874fbfadc11c83db771154b786010
Oliver Friedmann <oliver.friedmann@gmail.com>
2017-01-15 16:48:41 -0500

    More Grunt Fixes

--- db/grunt.js
+++ db/grunt.js
@@ -23,4 +23,77 @@ module.exports = [{
 		return descriptor.command.indexOf("grunt") >= 0 && descriptor.stdout.indexOf("You need to have Ruby and Sass installed") >= 0; 
 	}
 
+}, {
+	
+	"title": "Grunt without local NPM",
+	"description": "You forgot to npm install it.",
+	"commands": function (descriptor) {
+		return ["npm install", descriptor.command];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("grunt") >= 0 && (
+			descriptor.stdout.indexOf('Loading "Gruntfile.js" tasks...') >= 0 &&
+			descriptor.stdout.indexOf("Error: Cannot find module") >= 0); 
+	}
+	
+}, {
+	
+	"title": "Grunt missing module",
+	"description": "You forgot to npm install it.",
+	"commands": function (descriptor) {
+		var regex = /Error: Cannot find module '(.+)'/g;
+		var match = regex.exec(descriptor.stdout);
+		return ["npm install " + match[1], descriptor.command];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("grunt") >= 0 && (
+			descriptor.stdout.indexOf('Loading "Gruntfile.js" tasks...') >= 0 &&
+			descriptor.stdout.indexOf("Error: Cannot find module") >= 0); 
+	}
+	
+}, {
+	
+	"title": "Grunt missing module",
+	"description": "You forgot to npm link it.",
+	"commands": function (descriptor) {
+		var regex = /Error: Cannot find module '(.+)'/g;
+		var match = regex.exec(descriptor.stdout);
+		return ["npm link " + match[1], descriptor.command];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("grunt") >= 0 && (
+			descriptor.stdout.indexOf('Loading "Gruntfile.js" tasks...') >= 0 &&
+			descriptor.stdout.indexOf("Error: Cannot find module") >= 0); 
+	}
+	
+}, {
+	
+	"title": "Grunt missing module",
+	"description": "You forgot to npm install it.",
+	"commands": function (descriptor) {
+		var regex = /Local Npm module "(.+)"/g;
+		var match = regex.exec(descriptor.stdout);
+		return ["npm install " + match[1], descriptor.command];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("grunt") >= 0 && (
+			descriptor.stdout.indexOf('Loading "Gruntfile.js" tasks...') >= 0 &&
+			descriptor.stdout.indexOf("Local Npm module") >= 0); 
+	}
+	
+}, {
+	
+	"title": "Grunt missing module",
+	"description": "You forgot to npm link it.",
+	"commands": function (descriptor) {
+		var regex = /Local Npm module "(.+)"/g;
+		var match = regex.exec(descriptor.stdout);
+		return ["npm link " + match[1], descriptor.command];
+	},
+	"applies": function (descriptor) {
+		return descriptor.command.indexOf("grunt") >= 0 && (
+			descriptor.stdout.indexOf('Loading "Gruntfile.js" tasks...') >= 0 &&
+			descriptor.stdout.indexOf("Local Npm module") >= 0); 
+	}
+	
 }];
